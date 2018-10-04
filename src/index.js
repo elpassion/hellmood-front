09755910e9cf12c7b76f0187d5e05979b9ba00fa
bartektricks <bartektricks@gmail.com>
2018-10-04 11:00:51 +0200

    SCSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+WebFont.load({
+    google: {
+        families: ['Roboto:300,400,700']
+    }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
