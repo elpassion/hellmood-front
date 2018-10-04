e57250afc56913b1b40ac127691a61cb6bf475c2
Mariusz Heyda <insanepl@gmail.com>
2018-10-04 11:00:47 +0200

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.scss';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import './style.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
