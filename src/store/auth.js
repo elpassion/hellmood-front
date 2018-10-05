06ae1b2519fc2c7826dd22d114ebe35f5dfd8bbc
Mariusz Heyda <insanepl@gmail.com>
2018-10-05 10:34:31 +0200

    Authentication

--- src/store/auth.js
+++ src/store/auth.js
@@ -2,13 +2,17 @@ import { observable, action, reaction } from 'mobx';
 
 class AuthStore {
   @observable isAuthenticated = window.localStorage.getItem('isAuthenticated');
-  @observable token;
+  @observable token = window.localStorage.getItem('accessToken');
 
   @action login = (token) => {
     this.token = token;
     if (token) {
       this.isAuthenticated = true;
       window.localStorage.setItem('isAuthenticated', true);
+      window.localStorage.setItem('accessToken', token);
+    } else {
+      window.localStorage.removeItem('isAuthenticated');
+      window.localStorage.removeItem('accessToken');
     }
   }
 }
