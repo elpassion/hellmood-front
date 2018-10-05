52f8bbf65463a4cdc09ad3a82a4f59538e0dbac6
Mariusz Heyda <insanepl@gmail.com>
2018-10-05 09:29:55 +0200

    ENV TOKEN

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -6,11 +6,10 @@ import { inject, observer } from 'mobx-react';
 @inject('authStore')
 @observer
 class Login extends Component {
-
   handleSuccess = (response) => {
     const tokenID = response.tokenId;
 
-    axios.post('http://8c5b4a25.ngrok.io/api/v1/login', {
+    axios.post(`${process.env.PREACT_APP_API_URL}/api/v1/login`, {
       params: {
         tokenID
       }
@@ -23,7 +22,6 @@ class Login extends Component {
       })
       .then(function () {
       });
-
   };
 
   updateToken = (param) => {
